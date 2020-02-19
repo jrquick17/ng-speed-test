72b2b5aaa98ca1c818e40e3c25e685267c873b41
Jeremy Quick <jrquick@uab.edu>
2020-02-19 08:52:06 -0600

    Use image from personal server

--- src/app/services/speed-test.service.ts
+++ src/app/services/speed-test.service.ts
@@ -14,8 +14,7 @@ export class SpeedTestService {
       (observer) => {
         window.setTimeout(
           () => {
-            // const imageAddr = 'https://ng-speed-test.jrquick.com/assets/internet-speed-image.jpg';
-            const imageAddr = 'https://webapp.uic-chp.org/internet-speed-image.jpg';
+            const imageAddr = 'https://ng-speed-test.jrquick.com/assets/internet-speed-image.jpg';
 
             let startTime, endTime;
             const download = new Image();
