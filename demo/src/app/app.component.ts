3b9cb75a5d45d1460dea946e88b6f8536a431ed9
Jeremy Quick <jrquick@uab.edu>
2020-02-19 07:31:11 -0600

    Create simple demo

--- demo/src/app/app.component.ts
+++ demo/src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
+import {finalize} from 'rxjs/operators';
+
+import {SpeedTestService} from 'ng-speed-test';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-speed-test-demo';
+
+  public hasChecked:boolean = false;
+  public isChecking:boolean = false;
+  public speed:string|boolean = '';
+
+  constructor(
+    private speedTestService:SpeedTestService
+  ) {
+
+  }
+
+  getSpeed():void {
+    this.isChecking = true;
+
+    this.speedTestService.getSpeed().subscribe(
+      (speed) => {
+        this.speed = speed;
+
+        this.isChecking = false;
+        this.hasChecked = true;
+      }
+    )
+  }
 }
--- demo/src/app/app.module.ts
+++ demo/src/app/app.module.ts
@@ -3,14 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SpeedTestModule } from 'ng-speed-test';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    SpeedTestModule
   ],
   providers: [],
   bootstrap: [AppComponent]
