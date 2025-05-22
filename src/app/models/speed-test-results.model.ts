3f58fbd4765fc759bea83600987616d315fbabb0
Jeremy Quick <jeremy.quick@bullhorn.com>
2025-05-22 13:15:49 -0500

    Added TypeScript interfaces for better type safety Enhanced constructors with partial parameter support Added computed properties for speed conversions Better error handling in results model

--- src/app/models/speed-test-results.model.ts
+++ src/app/models/speed-test-results.model.ts
@@ -1,52 +1,59 @@
-export class SpeedTestResultsModel {
-  public duration:number  = 0;
-  public hasEnded:boolean = false;
+export interface SpeedTestResults {
+  duration: number;
+  hasEnded: boolean;
+  startTime: number | null;
+  endTime: number | null;
+  speedBps: number;
+  speedKbps: number;
+  speedMbps: number;
+}
 
-  public startTime:number = null;
-  public endTime:number = null;
+export class SpeedTestResultsModel implements SpeedTestResults {
+  public duration: number = 0;
+  public hasEnded: boolean = false;
+  public startTime: number | null = null;
+  public endTime: number | null = null;
+  public speedBps: number = 0;
 
-  public speedBps:number = 0;
+  constructor(private fileSize: number) {}
 
-  constructor(
-    private fileSize:number
-  ) {
-
+  get speedKbps(): number {
+    return this.speedBps / 1024;
   }
 
-  private _update():void {
-    if (this.endTime !== null) {
+  get speedMbps(): number {
+    return this.speedKbps / 1024;
+  }
+
+  private _update(): void {
+    if (this.endTime !== null && this.startTime !== null) {
       const milliseconds = this.endTime - this.startTime;
       if (milliseconds !== 0) {
         this.duration = milliseconds / 1000;
       }
 
       const bitsLoaded = this.fileSize * 8;
-
       this.speedBps = bitsLoaded / this.duration;
     }
   }
 
-  end():void {
+  end(): void {
     if (!this.hasEnded) {
       this.hasEnded = true;
-
-      this.endTime = (new Date()).getTime();
-
+      this.endTime = Date.now();
       this._update();
     }
   }
 
-  error():void {
+  error(): void {
     if (!this.hasEnded) {
       this.hasEnded = true;
-
       this.endTime = null;
-
       this._update();
     }
   }
 
-  start():void {
-    this.startTime = (new Date()).getTime();
+  start(): void {
+    this.startTime = Date.now();
   }
 }
--- src/app/models/speed-test-settings.model.ts
+++ src/app/models/speed-test-settings.model.ts
@@ -1,7 +1,22 @@
-import {SpeedTestFileModel} from './speed-test-file.model';
+import {SpeedTestFile, SpeedTestFileModel} from './speed-test-file.model';
 
-export class SpeedTestSettingsModel {
-  public iterations?:number = 3;
-  public file?:SpeedTestFileModel = new SpeedTestFileModel();
-  public retryDelay?:number = 500;
+export interface SpeedTestSettings {
+  iterations?: number;
+  file?: SpeedTestFile;
+  retryDelay?: number;
+}
+
+export class SpeedTestSettingsModel implements SpeedTestSettings {
+  public iterations?: number = 3;
+  public file?: SpeedTestFileModel = new SpeedTestFileModel();
+  public retryDelay?: number = 500;
+
+  constructor(settings?: Partial<SpeedTestSettings>) {
+    if (settings) {
+      Object.assign(this, settings);
+      if (settings.file) {
+        this.file = new SpeedTestFileModel(settings.file);
+      }
+    }
+  }
 }
