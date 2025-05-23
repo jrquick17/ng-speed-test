aee1317331ecac929f1a83905a8aee304af7a013
Jeremy Quick <jeremy.quick@bullhorn.com>
2025-05-22 21:13:16 -0500

    Optimize demo size

--- dist/app/models/speed-test-settings.model.d.ts
+++
@@ -1,6 +0,0 @@
-import { SpeedTestFileModel } from './speed-test-file.model';
-export declare class SpeedTestSettingsModel {
-    iterations?: number;
-    file?: SpeedTestFileModel;
-    retryDelay?: number;
-}
--- dist/ng-speed-test/speed-test.module.d.ts
+++
@@ -1,8 +0,0 @@
-import * as i0 from "@angular/core";
-import * as i1 from "@angular/common";
-import * as i2 from "@angular/forms";
-export declare class SpeedTestModule {
-    static ɵfac: i0.ɵɵFactoryDeclaration<SpeedTestModule, never>;
-    static ɵmod: i0.ɵɵNgModuleDeclaration<SpeedTestModule, never, [typeof i1.CommonModule, typeof i2.FormsModule], never>;
-    static ɵinj: i0.ɵɵInjectorDeclaration<SpeedTestModule>;
-}
